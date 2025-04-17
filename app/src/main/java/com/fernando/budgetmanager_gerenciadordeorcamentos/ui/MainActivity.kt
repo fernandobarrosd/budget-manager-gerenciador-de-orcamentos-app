2175421faba63dd5e7e058707b8f3770e58e8054
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-04-17 19:31:28 -0300

    Init android project

---
+++ app/src/main/java/com/fernando/budgetmanager_gerenciadordeorcamentos/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.fernando.budgetmanager_gerenciadordeorcamentos.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fernando.budgetmanager_gerenciadordeorcamentos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
