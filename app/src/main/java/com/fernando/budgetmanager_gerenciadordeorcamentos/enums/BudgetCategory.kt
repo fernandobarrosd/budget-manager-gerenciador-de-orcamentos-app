75f2cfb50fa1942e1a4b6459715a4d6de7e36009
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-04-19 16:33:23 -0300

    Add compileSdk in build.gradle, create domain entities and value objects and create exceptions

---
+++ app/src/main/java/com/fernando/budgetmanager_gerenciadordeorcamentos/enums/BudgetCategory.kt
@@ -0,0 +1,7 @@
+package com.fernando.budgetmanager_gerenciadordeorcamentos.enums
+
+enum class BudgetCategory(val value: String) {
+    COMPLETED("Completed"),
+    NOT_COMPLETED("Not completed"),
+    EXPIRED("Expired")
+}
---
+++ app/src/main/java/com/fernando/budgetmanager_gerenciadordeorcamentos/exceptions/BudgetItemsListEmptyException.kt
@@ -0,0 +1,3 @@
+package com.fernando.budgetmanager_gerenciadordeorcamentos.exceptions
+
+class BudgetItemsListEmptyException(override val message: String) : RuntimeException(message)
